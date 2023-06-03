d559833606c3eb6e177b4fa2a8d3a66209d47d24
Tanuki33 <aditiockw@gmail.com>
2023-06-03 20:08:48 +0700

    Changed to use onnxruntime-directml.

--- roop/core.py
+++ roop/core.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import platform
 import signal
 import sys
@@ -74,22 +72,7 @@ def pre_check():
     model_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../inswapper_128.onnx')
     if not os.path.isfile(model_path):
         quit('File "inswapper_128.onnx" does not exist!')
-    if '--gpu' in sys.argv:
-        NVIDIA_PROVIDERS = ['CUDAExecutionProvider', 'TensorrtExecutionProvider']
-        if len(list(set(roop.globals.providers) - set(NVIDIA_PROVIDERS))) == 1:
-            CUDA_VERSION = torch.version.cuda
-            CUDNN_VERSION = torch.backends.cudnn.version()
-            if not torch.cuda.is_available() or not CUDA_VERSION:
-                quit("You are using --gpu flag but CUDA isn't available or properly installed on your system.")
-            if CUDA_VERSION > '11.8':
-                quit(f"CUDA version {CUDA_VERSION} is not supported - please downgrade to 11.8")
-            if CUDA_VERSION < '11.4':
-                quit(f"CUDA version {CUDA_VERSION} is not supported - please upgrade to 11.8")
-            if CUDNN_VERSION < 8220:
-                quit(f"CUDNN version {CUDNN_VERSION} is not supported - please upgrade to 8.9.1")
-            if CUDNN_VERSION > 8910:
-                quit(f"CUDNN version {CUDNN_VERSION} is not supported - please downgrade to 8.9.1")
-    else:
+    if '--gpu' not in sys.argv:
         roop.globals.providers = ['CPUExecutionProvider']
     if '--all-faces' in sys.argv or '-a' in sys.argv:
         roop.globals.all_faces = True
--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from roop import core
 
 if __name__ == '__main__':
